d67c3abc30221ca8129669281c4bd314902af8b7
squid_ro <squid_ro@accton.com>
2022-03-22 07:05:06 +0000

    Update xdp_cutPacket.py to config sip filter list dynamically.

--- src/xdp_cutPacket.py
+++ src/xdp_cutPacket.py
@@ -3,7 +3,8 @@
 # Licensed under the Apache License, Version 2.0 (the "License")
 
 from bcc import BPF
-import pyroute2, time, sys, ctypes, socket, ipaddress, argparse
+from bcc import libbcc, table
+import pyroute2, time, sys, ctypes, socket, ipaddress, argparse, os
 
 c_text = """
 #include <uapi/linux/bpf.h>
@@ -14,12 +15,25 @@ c_text = """
 #include <linux/ip.h>
 #include <linux/ipv6.h>
 
-#if HT_MAX > 0
 //in network order
-BPF_HASH(sip_hash, u32, u32, HT_MAX);
-#endif
+BPF_HASH(sip_filter, u32, u32, HT_MAX);
 
 BPF_ARRAY(mod_total, uint64_t, 1);
+BPF_ARRAY(en_sip_filter, u32, 1);
+
+static inline int is_en_sip_filter(void)
+{
+    uint32_t *en_flag;
+
+    en_flag = en_sip_filter.lookup((uint32_t []) {0});
+
+    if (en_flag)
+    {
+        return (*en_flag != 0);
+    }
+
+    return 0;
+}
 
 static inline int parse_ipv4(void *data, u64 nh_off, void *data_end) {
     struct iphdr *iph = data + nh_off;
@@ -45,25 +59,6 @@ static inline int parse_ipv6(void *data, u64 nh_off, void *data_end) {
     if ((void*)&ip6h[1] > data_end)
         return 0;
     return ip6h->nexthdr;
-}
-
-static inline unsigned short checksum(unsigned short *buf, int bufsz) {
-    unsigned long sum = 0;
-
-    while (bufsz > 1) {
-        sum += *buf;
-        buf++;
-        bufsz -= 2;
-    }
-
-    if (bufsz == 1) {
-        sum += *(unsigned char *)buf;
-    }
-
-    sum = (sum & 0xffff) + (sum >> 16);
-    sum = (sum & 0xffff) + (sum >> 16);
-
-    return ~sum;
 }
 
 static inline void update_mod_c (void) {
@@ -118,26 +113,26 @@ int xdp_reset_packet(struct CTXTYPE *ctx) {
 
     if (h_proto == htons(ETH_P_IP)) {
 
-#if HT_MAX > 0
-        ip = data + nh_off;
+        if (is_en_sip_filter()) {
+            ip = data + nh_off;
 
-        if ((void*)&ip[1] > data_end)
-            return rc;
+            if ((void*)&ip[1] > data_end)
+                return rc;
 
-        // truncate packets only if sip is configured in sip_hash
-        {
-            uint32_t *count;
+            // truncate packets only if sip is configured in sip_filter
+            {
+                uint32_t *count;
 
-            count = sip_hash.lookup(&ip->saddr);
+                count = sip_filter.lookup(&ip->saddr);
 
-            if (! count) {
+                if (! count) {
 #ifdef ENABLE_DBG
-                bpf_trace_printk("pass sip - %lx\\n", ip->saddr);
+                    bpf_trace_printk("pass sip - %lx\\n", ip->saddr);
 #endif
-                return XDP_PASS;
+                    return XDP_PASS;
+                }
             }
         }
-#endif
 
         index = parse_ipv4(data, nh_off, data_end);
         ip_hlength = ipv4_len(data, nh_off, data_end);
@@ -180,41 +175,75 @@ int xdp_reset_packet(struct CTXTYPE *ctx) {
 }
 """
 
+dbg_path       = '/sys/fs/bpf/dbg_' + os.path.basename(os.path.splitext(__file__)[0])
+flags          = 0
+offload_device = None
+mode           = BPF.XDP
+max_filter_sips= 1024
+
+class PinnedTable(table.HashTable):
+    def __init__(self, map_path, keytype, leaftype, max_entries):
+        map_fd = libbcc.lib.bpf_obj_get(ctypes.c_char_p(map_path.encode('utf-8')))
+        if map_fd < 0:
+            raise ValueError("Failed to open eBPF map")
+
+        self.map_fd = map_fd
+        self.Key = keytype
+        self.Leaf = leaftype
+        self.max_entries = max_entries
+
+def set_filter_hosts(kpath, add_lst, del_lst):
+    try:
+        sip_tbl = PinnedTable(kpath, ctypes.c_uint32, ctypes.c_uint32, max_filter_sips)
+
+        if del_lst != None:
+            for sip in del_lst:
+                try:
+                    sip_tbl.pop(ctypes.c_uint32(socket.htonl(int(ipaddress.IPv4Address(sip)))))
+                except:
+                    pass
+
+        if add_lst != None:
+            for sip in add_lst:
+                sip_tbl[ctypes.c_uint32(socket.htonl(int(ipaddress.IPv4Address(sip))))] = \
+                    ctypes.c_uint32(1)
+
+    except:
+        print("Failed to configure SIP filter !!!")
+
+    else:
+        print("Configured SIP filter list:")
+        is_none = True
+        for sip in sip_tbl.keys():
+            is_none = False
+            print("\t{}".format(str(ipaddress.ip_address(socket.ntohl(sip.value)))))
+        if is_none:
+            print("\tNone")
+        print("")
+
 parser = argparse.ArgumentParser(description='Used to truncate the mirrored packets.')
-parser.add_argument('--hosts', dest='HOSTS', nargs='+', help='source ip addresses to filter')
-parser.add_argument('-d', '--dbg', dest='DBG', action="store_true", help='enable debug message')
-parser.add_argument('dev', help='device' )
+parser.add_argument('--hosts', dest='HOSTS', nargs='+',
+                    help='source ip addresses to filter')
+parser.add_argument('--no-hosts', dest='NHOSTS', nargs='+',
+                    help='remove source ip addresses configured')
+parser.add_argument('-l', '--list', dest='LIST', default=None, action="store_true",
+                    help='list source ip addresses configured')
+parser.add_argument('-d', '--dbg', dest='DBG', action="store_true",
+                    help='enable debug message')
+parser.add_argument('dev', nargs ='?',
+                    help='device (required if not used to toggle bpf debug message)')
 args = parser.parse_args()
 
-filter_sip = []
-
-if args.HOSTS:
-    for host in args.HOSTS:
-        filter_sip.append(int(ipaddress.IPv4Address(host)))
-
-device = args.dev
-flags = 0
-offload_device = None
-
-#if len(sys.argv) == 2:
-#    device = sys.argv[1]
-#elif len(sys.argv) == 3:
-#    device = sys.argv[2]
-
-if len(sys.argv) == 3:
-    if "-S" in sys.argv:
-        # XDP_FLAGS_SKB_MODE
-        flags |= BPF.XDP_FLAGS_SKB_MODE
-    if "-D" in sys.argv:
-        # XDP_FLAGS_DRV_MODE
-        flags |= BPF.XDP_FLAGS_DRV_MODE
-    if "-H" in sys.argv:
-        # XDP_FLAGS_HW_MODE
-        offload_device = device
-        flags |= BPF.XDP_FLAGS_HW_MODE
-
-mode = BPF.XDP
-#mode = BPF.SCHED_CLS
+if max_filter_sips > 0:
+    if args.HOSTS == None and args.NHOSTS == None and args.LIST == None:
+        if args.dev == None:
+            print("error: the following arguments are required: dev")
+            exit (1)
+        else:
+            device = args.dev
+    else:
+        set_filter_hosts(dbg_path, args.HOSTS, args.NHOSTS)
+        exit(0)
 
 if mode == BPF.XDP:
     ret = "XDP_DROP"
@@ -227,10 +256,31 @@ else:
 b = BPF(text=c_text, cflags=["-w", "-DKBUILD_MODNAME",
         "-DRETURNCODE=%s" % ret, "-DCTXTYPE=%s" % ctxtype,
         ["", "-DENABLE_DBG"][args.DBG],
-        "-DHT_MAX=%d" % len(filter_sip)],
+        "-DHT_MAX=%d" % max_filter_sips],
         device=offload_device, debug=[0, 0x18][args.DBG])
 
 fn = b.load_func("xdp_reset_packet", mode, offload_device)
+en_sip_filter = b.get_table("en_sip_filter")
+en_sip_filter[ctypes.c_uint32(0)] = ctypes.c_uint32(0)
+
+if max_filter_sips > 0:
+    sip_filter = b.get_table("sip_filter")
+
+    try:
+        if os.path.exists(dbg_path):
+            os.remove(dbg_path)
+
+        ret = libbcc.lib.bpf_obj_pin(sip_filter.map_fd, ctypes.c_char_p(dbg_path.encode('utf-8')))
+        if ret != 0:
+            raise Exception("Failed to pin map !!!")
+
+    except:
+        if args.DBG == 0:
+            print("SIP filter table in kernel can not be modified !!!")
+            print("Plz execute \"mount -t bpf none /sys/fs/bpf\" first !!!\n")
+    else:
+        en_sip_filter[ctypes.c_uint32(0)] = ctypes.c_uint32(1)
+
 
 if mode == BPF.XDP:
     b.attach_xdp(device, fn, flags)
@@ -243,13 +293,6 @@ else:
           parent="ffff:fff2", classid=1, direct_action=True)
 
 mod_tot = b.get_table("mod_total")
-
-if len(filter_sip) > 0:
-    sip_hash = b.get_table("sip_hash")
-
-    # configure sip of the packet to be modified
-    for tmp_ip in filter_sip:
-        sip_hash[ctypes.c_uint(socket.htonl(tmp_ip))] = ctypes.c_uint(1)
 
 print("Printing packet modification counter, hit CTRL+C to stop")
 while 1:
